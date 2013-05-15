b457c77cf509b0b436d85048a60fc8c8e55a3828
santaux <santaux@gmail.com>
2013-05-15 23:24:05 +0400

    Added some specification to Colonel::Builder class

--- lib/colonel/builder.rb
+++ lib/colonel/builder.rb
@@ -1,4 +1,4 @@
-# example:
+# @example
 # builder = Colonel::Builder.new
 # jobs = builder.parse
 module Colonel
@@ -8,14 +8,17 @@ module Colonel
       @all_flag = opts[:all_flag].nil? ? true : opts[:all_flag]
     end
 
+    # Gets the current user and use it to operate with crontab.
     def user
       @_user ||= `whoami`.chomp
     end
 
+    # Returns crontab object.
     def crontab
       @_crontab ||= Crontab.new(user)
     end
 
+    # Launchs current crontab parsing.
     def parse
       @jobs ||= []
       crontab.reject_useless.each do |line|
@@ -24,10 +27,13 @@ module Colonel
       @jobs
     end
 
+    # Updates crontab file with current jobs.
     def update_crontab
       crontab.update(@jobs)
     end
 
+    # Returns a job with specified index
+    # @param (Integer) id is index of the job into @jobs array
     def find_job(id)
       @jobs.select { |j| j.id == id.to_i }.first
     end
@@ -36,6 +42,8 @@ module Colonel
       @jobs.each_with_index { |j,i| return i if j.id == id.to_i }
     end
 
+    # Replace job with specified id
+    # @param (Integer) :id is index of the job into @jobs array
     # TODO: Refactor it! To complicated!
     def update_job(opts={})
       index = get_job_index(opts[:id])
@@ -43,10 +51,14 @@ module Colonel
       @jobs[index] = job.update(opts)
     end
 
+    # Adds a job to @jobs array
+    # @param (Parser::Schedule) :schedule is object of Parser::Schedule class
+    # @param (Parser::Command) :command is object of Parser::Command class
     def add_job(opts={})
       @jobs << Job.new( Parser::Schedule.new(opts[:schedule]), Parser::Command.new(opts[:command]))
     end
 
+    # Destroy job from @jobs array
     def destroy_job(id)
       index = get_job_index(id)
       @jobs.delete_at index
